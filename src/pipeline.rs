56d0194e8ddc6cbea73008d2cb3d4a6548184b49
Corey Lowman <clowman1993@gmail.com>
2023-05-02 16:09:56 -0400

    Upgrades

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,6 +1,4 @@
-use std::ops::ControlFlow;
-
-use rand::rngs::StdRng;
+use rand::{rngs::StdRng, SeedableRng};
 use rust_tokenizers::tokenizer::{SentencePieceBpeTokenizer, Tokenizer, TruncationStrategy};
 
 use dfdx::{
@@ -11,40 +9,18 @@ use dfdx::{
 
 use super::sampling;
 
-pub struct LlamaArgs {
-    /// Seed for device & sampling RNG.
-    pub seed: u64,
-
-    /// Root directory of the **converted** model. Use `convert.py` to convert the PyTorch `.bin` to
-    /// the correct format.
-    pub model: String,
-
-    /// Number of new tokens to generate for each prompt.
+#[derive(Debug, Clone, Copy)]
+pub struct LlamaConfig {
     pub num_tokens: usize,
-
-    /// Maximum number of **megabytes** available
-    /// to store *model* weights.
-    pub ram_limit_for_model: Option<usize>,
-
-    /// Disable the KV cache. This will slow computations down,
-    /// but reduce memory usage.
     pub disable_cache: bool,
-
-    /// Whether to do greedy sampling or top_p sampling.
     pub greedy: bool,
-
-    /// The top probability value when using non-greedy sampling
     pub top_p: f32,
-
-    /// The temperature value when using non-greedy sampling
     pub temperature: f32,
-
-    /// The number of tokens to consider when using non-greedy sampling.
     pub top_k: usize,
 }
 
 pub struct LlamaPipeline {
-    pub args: LlamaArgs,
+    pub cfg: LlamaConfig,
     pub device: super::modeling::Dev,
     pub llama: super::modeling::LlamaForCausalLM,
     pub tokenizer: SentencePieceBpeTokenizer,
@@ -54,15 +30,50 @@ pub struct LlamaPipeline {
 }
 
 impl LlamaPipeline {
-    pub fn new(args: LlamaArgs) -> Self {
-        todo!()
+    pub fn new(
+        model: String,
+        ram_limit_for_model: Option<usize>,
+        seed: u64,
+        cfg: LlamaConfig,
+    ) -> Self {
+        const MB: usize = 1_000_000;
+
+        let rng = StdRng::seed_from_u64(seed);
+        let device = super::modeling::Dev::seed_from_u64(seed);
+
+        let mut llama = super::loading::load_on_disk(model.clone());
+        let model_num_bytes = llama.num_bytes();
+        println!("Model size: {} MB", model_num_bytes / MB);
+
+        let max_bytes = ram_limit_for_model
+            .map(|x| x * MB)
+            .unwrap_or(model_num_bytes);
+        let unused_bytes = llama.deferred_load(max_bytes);
+        println!(
+            "{} MB of model parameters will be held in RAM.",
+            (max_bytes - unused_bytes) / MB
+        );
+
+        let tokenizer =
+            SentencePieceBpeTokenizer::from_file(model + "/tokenizer.model", false).unwrap();
+
+        Self {
+            cfg,
+            device,
+            llama,
+            tokenizer,
+            bos_token: 1,
+            eos_token: 2,
+            rng,
+        }
     }
 
-    pub fn generate(
+    pub fn generate<Prompt: Into<String>>(
         &mut self,
-        prompt: String,
-        num_tokens: usize,
+        prompt: Prompt,
     ) -> impl Iterator<Item = String> + '_ {
+        let prompt = prompt.into();
+
         let tokenized_input = self.tokenizer.encode_list(
             &[prompt.clone()],
             prompt.len(),
@@ -81,10 +92,9 @@ impl LlamaPipeline {
 
         let mut cache: Option<Vec<super::modeling::Cache<Const<1>, usize>>> = None;
 
-        let start = std::time::Instant::now();
-
-        (0..num_tokens)
-            .map(|i_token| {
+        let cfg = self.cfg.clone();
+        (0..cfg.num_tokens)
+            .map(move |_| {
                 let n_tokens = tokens.len();
                 let input_ids = match cache.as_ref() {
                     None => self
@@ -93,38 +103,28 @@ impl LlamaPipeline {
                     Some(_) => self.device.tensor([[*tokens.last().unwrap()]]).realize(),
                 };
                 let seq_len = input_ids.shape().1;
-                let out = self.llama.forward(input_ids, cache);
+                let out = self.llama.forward(input_ids, cache.clone());
                 let logits = out.0;
-                cache = (!self.args.disable_cache).then(|| out.1);
+                cache = (!cfg.disable_cache).then(|| out.1);
                 let vocab = logits.select(self.device.tensor([seq_len - 1]));
-                let new_token = if self.args.greedy {
+                let new_token = if cfg.greedy {
                     sampling::greedy(vocab.to_dtype::<f32>().as_vec())
                 } else {
-                    let probs =
-                        (vocab.to_dtype::<f32>() / self.args.temperature).softmax::<Axis<1>>();
-                    sampling::top_p(
-                        probs.as_vec(),
-                        self.args.top_p,
-                        self.args.top_k,
-                        &mut self.rng,
-                    )
+                    let probs = (vocab.to_dtype::<f32>() / cfg.temperature).softmax::<Axis<1>>();
+                    sampling::top_p(probs.as_vec(), cfg.top_p, cfg.top_k, &mut self.rng)
                 };
 
+                tokens.push(new_token);
+
                 if new_token == self.eos_token {
-                    std::ops::ControlFlow::Break(())
+                    None
                 } else if new_token == 13 {
-                    std::ops::ControlFlow::Continue("\n".into())
+                    Some("\n".into())
                 } else {
-                    std::ops::ControlFlow::Continue(self.tokenizer.decode(
-                        &[new_token as i64],
-                        true,
-                        false,
-                    ))
+                    Some(self.tokenizer.decode(&[new_token as i64], true, false))
                 }
             })
-            .map_while(|item| match item {
-                ControlFlow::Break(()) => None,
-                ControlFlow::Continue(item) => Some(item),
-            })
+            .take_while(Option::is_some)
+            .map(Option::unwrap)
     }
 }
